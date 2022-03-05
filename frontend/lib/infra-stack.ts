ad326360700e00aec5b1285c655fefe1199624f9
Spencer Mehta <spencermehta@gmail.com>
2022-03-05 17:47:55 +0000

    feat: add domain name to next cdk

--- frontend/lib/infra-stack.ts
+++ frontend/lib/infra-stack.ts
@@ -1,15 +1,17 @@
-import { NextJSLambdaEdge } from '@sls-next/cdk-construct'
-import { Stack, StackProps, Duration } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NextJSLambdaEdge } from "@sls-next/cdk-construct";
+import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new NextJSLambdaEdge(this, 'SpencerPortfolio', {
-      serverlessBuildOutDir: './build',
+    console.log(process.env.DOMAIN_CERTIFICATE_ARN);
+    new NextJSLambdaEdge(this, "SpencerPortfolio", {
+      serverlessBuildOutDir: "./build",
       runtime: Runtime.NODEJS_14_X,
       memory: 1024,
       timeout: Duration.seconds(30),
@@ -18,7 +20,15 @@ export class InfraStack extends Stack {
         apiLambda: `${id}Api`,
         defaultLambda: `Fn${id}`,
         imageLambda: `${id}Image`,
-      }
-    })
+      },
+      domain: {
+        domainNames: ["spencermehta.com"],
+        certificate: Certificate.fromCertificateArn(
+          this,
+          "Cert",
+          process.env.DOMAIN_CERTIFICATE_ARN ?? ""
+        ),
+      },
+    });
   }
 }
